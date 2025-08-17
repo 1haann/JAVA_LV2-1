d272426094925e3261ec33837ce3aa2fa7616424
Han <inte098@naver.com>
2025-08-17 17:26:04 +0900

    섹션4 - String 클래스 기본

---
+++ src/lang/immutable/test/ImmuableMyDateMain.java
@@ -0,0 +1,18 @@
+package lang.immutable.test;
+
+public class ImmuableMyDateMain {
+    public static void main(String[] args) {
+        //date1=x001, date2=x001
+        ImmuableMyDate date1 = new ImmuableMyDate(2024, 1, 1);
+        ImmuableMyDate date2 = date1;
+
+        System.out.println("date1 = " + date1);
+        System.out.println("date2 = " + date2);
+
+        System.out.println("2025 -> date1");
+        date1 = date1.withYear(2025);   //x002
+
+        System.out.println("date1 = " + date1); //x002
+        System.out.println("date2 = " + date2); //x001
+    }
+}
