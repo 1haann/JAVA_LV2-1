8bf4552b541573ab60cae5b9b4aa56681a7d7df9
Han <inte098@naver.com>
2025-08-22 22:38:46 +0900

    섹션4 - 불변 객체

--- src/lang/immutable/string/equals/StringEqualsMain2.java
+++ src/lang/immutable/string/equals/StringEqualsMain2.java
@@ -8,12 +8,12 @@ public class StringEqualsMain2 {
 
         String str3 = "hello";
         String str4 = "hello";
-        System.out.println("메서드 호출 비교1: " + isSame(str3, str4));
+        System.out.println("메서드 호출 비교2: " + isSame(str3, str4));
     }
 
     private static boolean isSame(String x, String y) {
         // new String()으로 생성한 값이 들어올 지, 리터럴로 대입한 값이 들어올 지 모르기에 문자열 비교는 equals를 사용.
-        // return x == y;
-        return x.equals(y);
+         return x == y;
+//        return x.equals(y);
     }
 }
