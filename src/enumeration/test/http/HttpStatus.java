474533a016a848bf4916b514cac1ee55f90af9e1
Han <inte098@naver.com>
2025-10-20 21:30:16 +0900

    섹션6 - 타임존(ZonedDateTime)

---
+++ src/enumeration/test/http/HttpStatus.java
@@ -0,0 +1,16 @@
+package enumeration.test.http;
+
+public enum HttpStatus {
+    OK(200,"OK"),
+    BAD_REQUEST(400,"Bad Request"),
+    NOT_FOUND(404,"Not_Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
